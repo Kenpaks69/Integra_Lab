ed781675a4ab444df8df6ea458d6804ed4f28416
Kenpaks69 <nerikenlouie@gmail.com>
2026-04-25 07:20:40 -0700

    Lab Activit:  Secure Student Records API

--- students/models.py
+++ students/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Student(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) 
     student_id = models.CharField(max_length=20, unique=True) 
     full_name = models.CharField(max_length=100) 
     email = models.EmailField(unique=True) 
--- students/views.py
+++ students/views.py
@@ -1,7 +1,19 @@
 from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer 
+from .permissions import IsAdminOrFaculty
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            permission_classes = [IsAdminUser]
+        
+        elif self.action in ['update', 'partial_update']:
+            permission_classes = [IsAdminOrFaculty]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
